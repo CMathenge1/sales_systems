5c802b86bb2cfff513a60166a67f90bb0fd3f6eb
Mine <carolinemathenge09@gmail.com>
2023-10-07 17:46:47 +0300

    updated

--- dbservice.py
+++ dbservice.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-
+from datetime import datetime
 conn=psycopg2.connect(
     database="my_duka", user='postgres', password='@Carol#2023',
     host='localhost', port= '5432'
@@ -21,11 +21,9 @@ conn=psycopg2.connect(
 #once you see the products, now redo the code and have a function called get_data that:
 #take in table_name as argument, fetches all records, return list of records
 
-def get_data(p):
-   
+def get_data(t_name):
     cursor = conn.cursor()
-    t="select * from "+ ""+p
-    cursor.execute(t)
+    cursor.execute(f"select * from {t_name}")
     list_of_records= cursor.fetchall()
     return list_of_records
 
@@ -40,21 +38,66 @@ def get_data(p):
 
 #you can use string formating on cursor.execute(f"select * from {p}")
 
-def insert_data(table_name,columns, values):
-    try:
-        cursor = conn.cursor()
-        placeholders= ','.join(['%s'] * len(values))
-        insert_query= f"INSERT INTO {table_name} ({','.join(columns)}) VALUES ({placeholders})"
-        cursor.execute(insert_query, values)
-        conn.commit()
-        print(f"Data inserted into {table_name} successfully")
-    except (Exception, psycopg2.Error) as error:
-        print(f"Failed to insert into {table_name}: {error}")
-    finally:
-        conn.close()
-# columns= ('productname', 'buying_price', 'selling_price', 'stock_quantity')
-# values= ('maize', 120, 220, 500)
+# def insert_data(table_name,columns, values):
+#     try:
+#         cursor = conn.cursor()
+#         placeholders= ','.join(['%s'] * len(values))
+#         insert_query= f"INSERT INTO {table_name} ({','.join(columns)}) VALUES ({placeholders})"
+#         cursor.execute(insert_query, values)
+#         conn.commit()
+#         print(f"Data inserted into {table_name} successfully")
+#     except (Exception, psycopg2.Error) as error:
+#         print(f"Failed to insert into {table_name}: {error}")
+#     finally:
+#         conn.close()
 
-# insert_data('products', columns, values)
+def ins_product(values):
+    cursor=conn.cursor()
+    cursor.execute("insert into products (productname, buying_price, selling_price, stock_quantity) values(%s,%s,%s,%s)", values)
+    conn.commit()
+
+def insert_sale(sal):
+    cursor=conn.cursor()
+    insert_query="insert into sales (pid,quantity,created_at) values(%s,%s,now())"
+    cursor.execute(insert_query,sal)
+    conn.commit()
+
+def calculate_profit():
+    cursor=conn.cursor()
+    profit_query="""Select DATE(created_at) as date,
+      sum((selling_price-buying_price)*quantity) as profit
+      from sales join products on sales.pid=products.id
+      group by date
+      order by date"""
+    cursor.execute(profit_query)
+    records= cursor.fetchall()
+    return(records)
+
+def check_email_exists(x):
+    cursor=conn.cursor()
+    cursor.execute("Select exists(select 1 from users where email=%s)", x)
+    exists= cursor.fetchone()[0]
+    return (exists)
+
+def email_password_match(email, password):
+    cursor= conn.cursor()
+    cursor.execute("Select id from users where email=%s and password=%s", (email, password))
+    result= cursor.fetchone()
+    if result is not None:
+        id= result[0]
+        return id
+    else:
+        return None
+    
+def create_users(full_name, email, password):
+    cursor= conn.cursor()
+    create_query= "insert into users (full_name, email, password) values(%s, %s,%s)"
+    cursor.execute(create_query, (full_name, email,password))
+    conn.commit()
+    
+# columns= ('productname', 'buying_price', 'selling_price', 'stock_quantity')
+# values= ('amaize', 120, 220, 500)
+
+# insert_product(values)
 # conn.close()
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, session
 from dbservice import get_data
-from dbservice import insert_data
+from dbservice import ins_product, calculate_profit
+from dbservice import insert_sale, create_users, check_email_exists, email_password_match
 app=Flask(__name__)
+
+
+# def login_check():
+#     if session['email'] !=None:
+#         return redirect ("/dashboard")
+#     return redirect("/login")
+
 
 @app.route("/")
 def sales_system():
@@ -9,17 +17,97 @@ def sales_system():
 
 @app.route("/dashboard")
 def dashboad():
-    return render_template("dashboard.html")
+    dates=[]
+    profits=[]
+    for i in calculate_profit():
+        dates.append(str(i[0]))
+        profits.append(float(i[1]))
+    return render_template("dashboard.html", dates=dates, profits=profits)
 
 @app.route("/products")
 def products():
     prods=get_data("products")
     return render_template("products.html", myproducts=prods)
 
+@app.route("/insert_product", methods=["Post"])
+def insert_product():
+    name= request.form['productname']
+    bprice= request.form['buying_price']
+    sprice= request.form['selling_price']
+    squantity= request.form['stock_quantity']
+    v=(name, bprice,sprice,squantity)
+    ins_product(v)
+    return redirect ("/products")
 
 @app.route("/sales")
 def sales():
+    # login_check()
+    products= get_data("products")
     sales= get_data("sales")
-    return render_template("sales.html", mysales=sales)
+    return render_template("sales.html", mysales=sales, myproducts=products)
 
-app.run()
+@app.route("/add-sales", methods=["POST","get"])
+def insert_sales():
+    product_id= request.form["product_id"]
+    quantity= request.form["quantity"]
+    s= (product_id, quantity)
+    insert_sale(s)
+    return redirect ("/sales")
+
+
+@app.route("/register", methods=["Post","get"])
+def register():
+    full_name= request.form ["full_name"]
+    email= request.form ["email"]
+    password= request.form ["password"]
+    create_users(full_name,email,password)
+
+    if not check_email_exists(email):
+        create_users(full_name,email,password)
+        return redirect("/login")
+    else:
+        flash("email already exists")
+        return redirect ("/login")
+    
+@app.route("/login", methods=["Post","get"])
+def login():
+    # session['email']= request.form['email']
+    # return render_template("/dashboard")
+    
+    if request.method== "Post":
+        email= request.form ["email"]
+        password= request.form["password"]
+        user_id= email_password_match(email, password)
+
+        if user_id:
+            session["user_id"] = user_id
+            return redirect ("/dashboard")
+        else:
+            flash("Login failed; username or password is not correct ")
+    return render_template ("login.html")
+
+
+
+@app.route("/logout")
+def logout():
+    session.pop("email")
+    return redirect("/dashboard.html")
+
+
+    
+
+
+    
+    
+
+
+# @app.route("/insert_sale", methods=["Post"])
+# def insert_sale():
+#     pid= request.form['pid']
+#     q= request.form['quantity']
+#     time= request.form['created_at']
+#     S=(pid,q, time)
+#     insert_product(S)
+#     return redirect ("/sales")
+
+app.run(debug= True)
